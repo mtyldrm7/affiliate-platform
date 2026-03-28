375a816848a2a7a804124ce7de5f3e2af59ade42
mtyldrm7 <mtyldrm7@gmail.com>
2026-03-28 09:40:46 +0300

    remove procfile use flask

--- app.py
+++ app.py
@@ -244,6 +244,5 @@ def order_webhook():
 if not os.path.exists(DATABASE):
     init_db()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+port = int(os.environ.get('PORT', 8080))
+app.run(host='0.0.0.0', port=port)
